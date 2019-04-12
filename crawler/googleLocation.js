3a94e95edec3bb2924cba4f597c79167d9810d18
jim63 <b99611015@g.ntu.edu.tw>
2019-04-12 11:12:55 +0800

    gstore

--- crawler/googleLocation.js
+++ crawler/googleLocation.js
@@ -7,11 +7,10 @@ app.set('view engine', 'pug');
 app.use(express.static('public'));
 app.set('view engine', 'pug');
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
-
 
 let port = 3000;
 app.listen(port, () => {
@@ -29,7 +28,6 @@ app.get('/attraction', (req, res) => {
     },
     (e, r, b) => {
       if (!e) {
-        // res.json({ data: `${b}` });
         res.send(`${b}`);
       }
     }
@@ -47,7 +45,23 @@ app.get('/park', (req, res) => {
     },
     (e, r, b) => {
       if (!e) {
-        // res.json({ data: `${b}` });
+        res.send(`${b}`);
+      }
+    }
+  );
+});
+
+app.get('/store', (req, res) => {
+  let storeid = req.query.storeid;
+  console.log('park', location);
+  request(
+    {
+      url: `https://maps.googleapis.com/maps/api/place/details/json?key=AIzaSyC8qLjFTmMo0etL1tVO3OdLBrEKx41Qko0&placeid=${storeid}`,
+      method: 'GET',
+      'Content-Type': 'application/json;charset=UTF-8'
+    },
+    (e, r, b) => {
+      if (!e) {
         res.send(`${b}`);
       }
     }
