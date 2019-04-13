97b4262af72eb7b18872f358f76f72c24536b1f8
jim63 <b99611015@g.ntu.edu.tw>
2019-04-13 21:32:15 +0800

    homepage

--- frontend/pages/src/App.js
+++ frontend/pages/src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Header />
+        {/* <Header /> */}
         <Banner />
         <div className='contentsContainer'>
           <Nav />
--- frontend/pages/src/component/Banner.js
+++ frontend/pages/src/component/Banner.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 import './Banner.css';
+import logo from '../img/logo.png';
 
 class Banner extends Component {
   render() {
     return (
       <nav className='banner' style={{}}>
+        {/* <img className='bannerLogo' srcSet='../img/logo.jpg' width='600px' height='300px' alt='' srcset='' /> */}
+        <img className='bannerLogo' srcSet={logo} />
         <div className='bannerText' style={{}}>
-          <img src='' alt='' />
-          <p style={{}}>Work Life Balance</p>
-          <br />
-          <p style={{}}>Are you ready?</p>
+          <p>WORK LIFE BALANCE</p>
+          <p>ARE YOU READY?</p>
         </div>
       </nav>
     );
