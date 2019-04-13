7620b8cc39b05c3d5d2a145e846519e2d9f22772
jim63 <b99611015@g.ntu.edu.tw>
2019-04-13 23:27:36 +0800

    logo

--- frontend/pages/src/component/Banner.js
+++ frontend/pages/src/component/Banner.js
@@ -7,7 +7,9 @@ class Banner extends Component {
     return (
       <nav className='banner' style={{}}>
         {/* <img className='bannerLogo' srcSet='../img/logo.jpg' width='600px' height='300px' alt='' srcset='' /> */}
-        <img className='bannerLogo' srcSet={logo} />
+        {/* <img className='bannerLogo' srcSet={logo} /> */}
+        <img className='bannerLogo' src={'./logo.png'} />
+
         <div className='bannerText' style={{}}>
           <p>WORK LIFE BALANCE</p>
           <p>ARE YOU READY?</p>
