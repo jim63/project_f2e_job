d2ae02747d37e342850963876bb6605fa7fdb82b
jim63 <b99611015@g.ntu.edu.tw>
2019-05-05 21:42:10 +0800

    ban_bgc

---
+++ crawler/public/precache-manifest.c35a8bae08d85bf0379be884d77b0a17.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "05e6387086c32866ab9c7de4985d8839",
+    "url": "/static/media/104logo.05e63870.png"
+  },
+  {
+    "revision": "4fcc0aebd1309cabeedf",
+    "url": "/static/css/main.fc4f058f.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "83c670c2f1bf04d42872",
+    "url": "/static/js/2.198b235b.chunk.js"
+  },
+  {
+    "revision": "eb1babd863d1df457825d9aa858e42a6",
+    "url": "/static/media/default_104.eb1babd8.png"
+  },
+  {
+    "revision": "b031e74d0b3bdb1a80ae68d1334cf698",
+    "url": "/static/media/default_yourator.b031e74d.png"
+  },
+  {
+    "revision": "8ab76751f544e55010cec634761c62f2",
+    "url": "/static/media/banner-18.8ab76751.png"
+  },
+  {
+    "revision": "4fcc0aebd1309cabeedf",
+    "url": "/static/js/main.19a030b4.chunk.js"
+  },
+  {
+    "revision": "d36356688d472a66376f528a9501ac68",
+    "url": "/static/media/meetjobslogo.d3635668.svg"
+  },
+  {
+    "revision": "4246f79d0965d01fd194568c5612d03f",
+    "url": "/static/media/logo-1.4246f79d.svg"
+  },
+  {
+    "revision": "b04b2ce455053e2a5f29972913d28386",
+    "url": "/static/media/banner2.b04b2ce4.jpeg"
+  },
+  {
+    "revision": "6dedf6a678114bfb61b49bd72dade5c0",
+    "url": "/static/media/banner3.6dedf6a6.jpeg"
+  },
+  {
+    "revision": "edc4ee3391b8ec9ef4298f6ba67ff405",
+    "url": "/static/media/logo-2.edc4ee33.svg"
+  },
+  {
+    "revision": "63efa11ca755e25722b880a7ac2ddba2",
+    "url": "/index.html"
+  }
+];
